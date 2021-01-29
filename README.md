b47fc8aea7375dfb55e85d22db2284972022d795
Chetna Pannu <chetna.pannu@intelliware.ca>
2021-01-29 14:59:34 -0500

    Initial commit for ml5 style transfer

--- README.md
+++ README.md
@@ -1,2 +1,13 @@
-# ml5-boilerplate: with p5.js
-A basic html, css, javascript boilerplate for working building a project with ml5.js
+## Introduction
+Image style transfer using ML5.js. 
+
+Style Transfer is a machine learning technique that allows to transfer the style of one image into another one. This is a two step process, first you need to train a model on one particular style and then you can apply this style to another image.
+
+Three pretrained models have been added from https://github.com/ml5js/ml5-data-and-models-server under ```models``` directory.
+
+This repo is based on the example provided here https://github.com/ml5js/ml5-library/tree/main/examples/javascript/StyleTransfer/StyleTransfer_Image
+
+## Run app
+* Clone repo 
+* Start a local web server using Python 2.7 ```python -m SimpleHTTPServer 8081``` or using sing Python 3 ```python -m http.server 8081```
+* Go to http://localhost:8081/
--- sketch.js
+++ sketch.js
@@ -1,12 +1,49 @@
-// Your code will go here
-// open up your console - if everything loaded properly you should see 0.3.0
-console.log('ml5 version:', ml5.version);
+let inputImg;
+let statusMsg;
+let transferBtn;
+let style1;
+let style2;
+let style3;
 
-function setup(){
-    createCanvas(400, 400);
+function setup() {
+  noCanvas();
+  // Status Msg
+  statusMsg = select("#statusMsg");
+
+  // Get the input image
+  inputImg = select("#inputImg");
+
+  // Transfer Button
+  transferBtn = select("#transferBtn");
+  transferBtn.mousePressed(transferImages);
+
+  // Create two Style methods with different pre-trained models
+  style1 = ml5.styleTransfer("models/style-transfer/wave", modelLoaded);
+  style2 = ml5.styleTransfer("models/style-transfer/udnie", modelLoaded);
+  style3 = ml5.styleTransfer("models/style-transfer/scream", modelLoaded);
 }
 
-function draw(){
-    background(200);
-    
+// A function to be called when the models have loaded
+function modelLoaded() {
+  // Check if both models are loaded
+  if (style1.ready && style2.ready) {
+    statusMsg.html("Ready!");
+  }
+}
+
+// Apply the transfer to both images!
+function transferImages() {
+  style1.transfer(inputImg, function (err, result) {
+    createImg(result.src).parent("styleATransfered");
+  });
+
+  style2.transfer(inputImg, function (err, result) {
+    createImg(result.src).parent("styleBTransfered");
+  });
+
+  style3.transfer(inputImg, function (err, result) {
+    createImg(result.src).parent("styleCTransfered");
+  });
+
+  statusMsg.html("Done!");
 }
